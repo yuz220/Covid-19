ad2cf71c1b0d222a29a5392679d4b71c862b8a99
yuz220 <53377520+yuz220@users.noreply.github.com>
2021-02-03 15:13:08 -0500

    New update in shiny app

---
+++ Covid-19/server.R
@@ -0,0 +1,115 @@
+library(shiny)
+library(plotly)
+library(COVID19)
+library(ggplot2)
+library(thematic)
+library(ggstance)
+library(broom.mixed)
+library(car)
+
+server <- function(input, output) {
+    
+    observeEvent(input$reset, {
+        shinyjs::reset("country") 
+        shinyjs::reset("type") 
+        shinyjs::reset("level") 
+        shinyjs::reset("date") 
+        shinyjs::reset("plotType")
+        
+    })
+    
+    newData <- reactive({
+        mydataset <- covid19(country = input$country, level = input$level,
+                start = input$date[1], end = input$date[2])
+    })
+    
+##############################################
+# Scatter plot
+    
+    output$plot1 <- renderPlotly({ 
+        if(!is.null(input$country)){
+            
+            mydata <- newData()
+            
+            color <- paste0("administrative_area_level_", input$level)
+         
+            if (input$plotType == "plots") {
+                
+                fig <- plot_ly(x = mydata[["date"]], y = mydata[[input$type]], 
+                               color = mydata[[color]])
+                
+                m <- list(l = 50, r = 50, b = 100, t = 100, pad = 4)
+                f14 <- list(family = "sans serif", size = 14, color = 'Black')
+                f12 <- list(family = "sans serif", size = 12, color = 'Black')
+                
+                fig <- fig %>% layout(title = 'Covid19 Cases for All Countries and Regions by Date (Scatter plot)',
+                                      xaxis = list(title = 'Date', font = f12),
+                                      yaxis = list(title = 'Number of cases', font = f12), 
+                                      font = f14, margin = m)
+                
+                fig
+            }
+        }
+    })
+
+    
+##############################################
+# Bar plot
+    
+    output$plot2 <- renderPlotly({
+
+        if(!is.null(input$country)){
+            
+            mydata <- newData()
+            color <- paste0("administrative_area_level_", input$level)
+
+            if (input$plotType == "plots") {
+                
+                Countries <- mydata[[color]]
+                
+                p <- ggplot(mydata, aes(x=mydata[["date"]], y=mydata[[input$type]], fill = Countries)) + 
+                    geom_bar(stat='identity') + coord_flip() + theme_minimal() +
+                    ggtitle("Covid19 Cases for All Countries and Regions by Date (Bar plot)") +
+                    xlab("Date") + ylab("Number of observations")
+
+                p + theme(
+                    plot.margin = margin(2, 2, 0, 0, "cm"),
+                    plot.title = element_text(family = "sans serif", size = 14, color = 'Black'),
+                    axis.title.x = element_text(family = "sans serif", size = 12, color = 'Black'),
+                    axis.title.y = element_text(family = "sans serif", size = 12, color = 'Black') 
+                ) 
+            }
+        }
+    })
+    
+    
+##############################################
+# Regression Analysis   
+    
+    output$summary <- renderPrint({
+        
+        if (input$plotType == "RegressionAnalysis") {
+            
+            mydata <- newData()
+
+            m1 <- lm(confirmed ~ school_closing + workplace_closing + 
+                         cancel_events + gatherings_restrictions + transport_closing + 
+                         stay_home_restrictions + internal_movement_restrictions + 
+                         international_movement_restrictions + information_campaigns + 
+                         testing_policy + contact_tracing, data = mydata)
+            
+            summary(m1)
+        }
+    })
+
+    
+##############################################
+# References & why this porject is meaningful
+    
+    output$Reference <- renderText({ 
+        paste("The dataset used in this project is from package'COVID19'. This project visualize the various kinds of cases of Covid-19 for all countries and regions.
+              It also presents the relation of cases and the government policies 
+              which gives us a clear view of the efficiency of the related policies in covid-19 prevention.")
+    })
+    
+}
---
+++ Covid-19/ui.R
@@ -0,0 +1,59 @@
+library(shiny)
+library(plotly)
+library(COVID19)
+library(shinyjs)
+
+ui <- fluidPage(
+    
+    useShinyjs(),
+    actionButton("reset", "Clear"), 
+    
+    titlePanel("Covid-19 Geographic Analytics"),
+    
+    sidebarLayout(
+        sidebarPanel(
+            
+            h4("Introduction"),
+            p("This is a Covid-19 analytics by all countries and regions in 2020. 
+              Please check all Selectboxes below to get the resulting plots or analysis:"),
+            
+            radioButtons("plotType", label = h3("Plot Type"),
+                         c(Plots = "plots",  RegressionAnalysis = "RegressionAnalysis")),
+            
+            selectInput("country", label = "Country", multiple = TRUE,
+                        choices = unique(covid19()$administrative_area_level_1),
+                        selected = "Canada"),
+
+            conditionalPanel(
+                condition = "input.plotType == 'plots'", 
+                
+                selectInput("type", label = "Type of Cases",
+                                choices = c("confirmed", "deaths", "tests", "recovered")),
+                
+                selectInput("level", label = "Granularity", 
+                                choices = c("Country" = 1, "Region" = 2), selected = 1)
+            ),
+            
+            dateRangeInput("date", label = "Date", start = "2020-06-01", end = "2020-08-31")
+        ),
+        
+        mainPanel(
+            tabsetPanel(type = "tabs",
+                        
+                        tabPanel("Scatter Plot", plotlyOutput("plot1")),
+                        tabPanel("Bar Plot", plotlyOutput("plot2")),
+
+                        tabPanel(
+                            "Regression Analysis", 
+                            conditionalPanel(
+                                condition = "input.plotType == 'RegressionAnalysis'",
+                                verbatimTextOutput("summary")),
+                        ), 
+                        
+                        tabPanel("Reference", textOutput("Reference"))
+                
+            )
+        )
+    )
+)
+
